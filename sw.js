90c2930fe9d1f8d03412b864117cb890ad61bcfe
hamadine <212258452+hamadine@users.noreply.github.com>
2025-05-28 01:30:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,42 @@
 const cacheName = "tadaksahak-v1";
+
 const assets = [
   "./",
   "./index.html",
-  "./style.css
+  "./style.css",
   "./manifest.json",
+  "./images/idaksahak_round.png",
   "./icon-192.png",
   "./icon-512.png",
-  "./data/mots.json",
-  "./stylelintrc.JSON
+  "./data/mots.json"
+  // "./stylelintrc.JSON"  <-- Probablement inutile ici (explication ci-dessous)
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
+// INSTALLATION : mise en cache des fichiers essentiels
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+// ACTIVATION : nettoyage des anciens caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// FETCH : réponse par cache ou requête réseau
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
     })
   );
 });
