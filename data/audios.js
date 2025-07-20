899e5026e24a710fd401308645bcf74ea75d7979
hamadine <212258452+hamadine@users.noreply.github.com>
2025-07-20 21:16:49 +0100

    Update audios.js

--- data/audios.js
+++ data/audios.js
@@ -1,8 +1,10 @@
 const audios = [
-  
   {
     album: "Hamadine",
     pistes: [
+      { title: "À chaque instant Tamalat – Hamadine", src: "data/audio/Hamadine/a_chaque_instant_tamalat_by_hamadine.mp3" },
+      { title: "Hamadine – Dis-moi", src: "data/audio/Hamadine/hamadine_dis_moi.mp3" },
+      { title: "Hommage à Moussa", src: "data/audio/Hamadine/hommage_a_moussa.mp3" },
       { title: "Rien qu’un homme", src: "data/audio/Hamadine/rien_qu_un_homme.mp3" },
       { title: "Hamadine raconte", src: "data/audio/Hamadine/hamadine_raconte.mp3" },
       { title: "More than surviving", src: "data/audio/Hamadine/more_than_surviving.mp3" },
