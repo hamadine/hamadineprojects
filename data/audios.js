30c244c466ad1469b802f625e58f106093a35992
hamadine <212258452+hamadine@users.noreply.github.com>
2025-07-20 20:31:02 +0100

    Update audios.js

--- data/audios.js
+++ data/audios.js
@@ -1,20 +1,11 @@
 const audios = [
-  {
-    album: "Idaksahak",
-    pistes: [
-      { title: "Hamadine – Dis-moi", src: "data/audio/Idaksahak/hamadine_dis-moi.mp3" },
-      { title: "À chaque instant Tamalat – Hamadine", src: "data/audio/Idaksahak/le_désert_a_un_nom.mp3" },
-      { title: "الصحراء لها اسم", src: "data/audio/Idaksahak/2_الصحراء_لها_اسم.mp3" },
-      { title: "Hommage à Moussa", src: "data/audio/Idaksahak/hommage_à__Moussa_.mp3" },
-      { title: "Idakssahak", src: "data/audio/Idaksahak/idakssahak_(1).mp3" }
-    ]
-  },
+  
   {
     album: "Hamadine",
     pistes: [
       { title: "Rien qu’un homme", src: "data/audio/Hamadine/rien_qu_un_homme.mp3" },
       { title: "Hamadine raconte", src: "data/audio/Hamadine/hamadine_raconte.mp3" },
-      { title: "More than surviving (official)", src: "data/audio/Hamadine/more_than_surviving.mp3" },
+      { title: "More than surviving", src: "data/audio/Hamadine/more_than_surviving.mp3" },
       { title: "Rose (English version)", src: "data/audio/Hamadine/rose_english_version.mp3" },
       { title: "Tabalit", src: "data/audio/Hamadine/tabalit.mp3" }
     ]
