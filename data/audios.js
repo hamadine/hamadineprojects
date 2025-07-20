4842b68e55a3d3811c0382268d7cc50c14a09d5a
hamadine <212258452+hamadine@users.noreply.github.com>
2025-07-20 17:11:54 +0100

    Create audios.js

---
+++ data/audios.js
@@ -0,0 +1,22 @@
+const audios = [
+  {
+    album: "Idaksahak",
+    pistes: [
+      { title: "Hamadine – Dis-moi", src: "data/audio/album1/Hamadine Dis-moi.mp3" },
+      { title: "À chaque instant Tamalat – Hamadine", src: "data/audio/album1/A chaque instant Tamalat - Hamadine.mp3" },
+      { title: "الصحراء لها اسم", src: "data/audio/album1/الصحراء لها اسم 2.mp3" },
+      { title: "Hommage à Moussa", src: "data/audio/album1/Hommage à Moussa .mp3" },
+      { title: "Idakssahak", src: "data/audio/album1/Idakssahak (1).mp3" }
+    ]
+  },
+  {
+    album: "Hamadine",
+    pistes: [
+      { title: "Rien qu’un homme", src: "data/audio/album2/Rien qu'un homme2(1).mp3" },
+      { title: "Hamadine raconte", src: "data/audio/album2/Hamadine_raconte.mp3" },
+      { title: "More than surviving (official)", src: "data/audio/album2/More than surviving official .mp3" },
+      { title: "Rose (English version)", src: "data/audio/album2/Rose English version mp3.mp3" },
+      { title: "Tabalit", src: "data/audio/album2/Tabalit.mp3" }
+    ]
+  }
+];
