1737096f7295d79c9bc20f6466343efc12f9509f
hamadine <212258452+hamadine@users.noreply.github.com>
2025-07-20 18:47:50 +0100

    Update audios.js

--- data/audios.js
+++ data/audios.js
@@ -1,20 +1,22 @@
-{
-  album: "Hamadine",
-  pistes: [
-    { title: "Rien qu’un homme", src: "data/audio/Hamadine/rien_qu'un_homme.mp3" },
-    { title: "Hamadine raconte", src: "data/audio/Hamadine/hamadine_raconte_.mp3" },
-    { title: "More than surviving (official)", src: "data/audio/Hamadine/More_than_surviving_.mp3" },
-    { title: "Rose (English version)", src: "data/audio/Hamadine/rose_english_version.mp3" },
-    { title: "Tabalit", src: "data/audio/Hamadine/tabalit_.mp3" }
-  ]
-}
-{
-  album: "Idaksahak",
-  pistes: [
-    { title: "Hamadine – Dis-moi", src: "data/audio/Idaksahak/hamadine_dis_moi.mp3" },
-    { title: "À chaque instant Tamalat – Hamadine", src: "data/audio/Idaksahak/a_chaque_instant_tamalat.mp3" },
-    { title: "الصحراء لها اسم", src: "data/audio/Idaksahak/sahara_laha_ism.mp3" },
-    { title: "Hommage à Moussa", src: "data/audio/Idaksahak/hommage_moussa.mp3" },
-    { title: "Idakssahak", src: "data/audio/Idaksahak/idakssahak.mp3" }
-  ]
-}
+const audios = [
+  {
+    album: "Idaksahak",
+    pistes: [
+      { title: "Hamadine – Dis-moi", src: "data/audio/Idaksahak/hamadine_dis_moi.mp3" },
+      { title: "À chaque instant Tamalat – Hamadine", src: "data/audio/Idaksahak/a_chaque_instant_tamalat.mp3" },
+      { title: "الصحراء لها اسم", src: "data/audio/Idaksahak/sahara_laha_ism.mp3" },
+      { title: "Hommage à Moussa", src: "data/audio/Idaksahak/hommage_moussa.mp3" },
+      { title: "Idakssahak", src: "data/audio/Idaksahak/idakssahak.mp3" }
+    ]
+  },
+  {
+    album: "Hamadine",
+    pistes: [
+      { title: "Rien qu’un homme", src: "data/audio/Hamadine/rien_qu_un_homme.mp3" },
+      { title: "Hamadine raconte", src: "data/audio/Hamadine/hamadine_raconte.mp3" },
+      { title: "More than surviving (official)", src: "data/audio/Hamadine/more_than_surviving.mp3" },
+      { title: "Rose (English version)", src: "data/audio/Hamadine/rose_english_version.mp3" },
+      { title: "Tabalit", src: "data/audio/Hamadine/tabalit.mp3" }
+    ]
+  }
+];
