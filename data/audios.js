01b014c272bbc1d7f01962bce027b03928154302
hamadine <212258452+hamadine@users.noreply.github.com>
2025-07-21 08:39:29 +0100

    Update audios.js

--- data/audios.js
+++ data/audios.js
@@ -6,6 +6,7 @@ const audios = [
       { title: "Hamadine – Dis-moi", src: "data/audio/Hamadine/hamadine_dis_moi.mp3" },
       { title: "Hommage à Moussa", src: "data/audio/Hamadine/hommage_a_moussa.mp3" },
       { title: "Idakssahak", src: "data/audio/Hamadine/idaksahak.mp3" },
+      { title: "Voix du lien (Bridge of Hope)", src: "data/audio/Hamadine/voix_du_lien_bridge_of_hope.mp3" },
       { title: "Rien qu’un homme", src: "data/audio/Hamadine/rien_qu_un_homme.mp3" },
       { title: "Hamadine raconte", src: "data/audio/Hamadine/hamadine_raconte.mp3" },
       { title: "More than surviving", src: "data/audio/Hamadine/more-than-surviving.mp3" },
