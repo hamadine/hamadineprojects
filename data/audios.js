a9807f3565691be7140aeff9b4b937e9fceb4a6e
hamadine <212258452+hamadine@users.noreply.github.com>
2025-07-29 17:09:13 +0100

    Update audios.js
    updated with less

--- data/audios.js
+++ data/audios.js
@@ -3,12 +3,6 @@ const audios = [
     album: "Hamadine",
     pistes: [
       { title: "Ce que Dieu m’a laissé", src: "data/audio/Hamadine/ce_que_dieu_m_a_laisse.mp3" },
-      { title: "Ton daron, ton monde Amira", src: "data/audio/Hamadine/ton_daron_ton_monde_amira.mp3" },
-      { title: "Je jure devant Dieu", src: "data/audio/Hamadine/je_jure_devant_dieu.mp3" },
-      { title: "Je n'ai pas choisi l'amour, j'ai choisi mes enfants", src: "data/audio/Hamadine/je_n_ai_pas_choisi_l_amour_j_ai_choisi_mes_enfants.mp3" },
-      { title: "Trois femmes, un cœur", src: "data/audio/Hamadine/trois_femmes_un_coeur.mp3" },
-      { title: "Trois femmes, trois flammes, un homme", src: "data/audio/Hamadine/trois_femmes_trois_flammes_un_homme.mp3" },
-      { title: "Pas une cicatrice, un langage", src: "data/audio/Hamadine/pas_une_cicatrice_un_langage.mp3" },
       { title: "À chaque instant Tamalat – Hamadine", src: "data/audio/Hamadine/a_chaque_instant_tamalat_by_hamadine.mp3" },
       { title: "Hamadine – Dis-moi", src: "data/audio/Hamadine/hamadine_dis_moi.mp3" },
       { title: "Hommage à Moussa", src: "data/audio/Hamadine/hommage_a_moussa.mp3" },
@@ -16,9 +10,7 @@ const audios = [
       { title: "Voix du lien (Bridge of Hope)", src: "data/audio/Hamadine/voix_du_lien_bridge_of_hope.mp3" },
       { title: "Rien qu’un homme", src: "data/audio/Hamadine/rien_qu_un_homme.mp3" },
       { title: "Hamadine raconte", src: "data/audio/Hamadine/hamadine_raconte.mp3" },
-      { title: "More than surviving", src: "data/audio/Hamadine/more-than-surviving.mp3" },
-      { title: "Rose (English version)", src: "data/audio/Hamadine/rose_english_version.mp3" },
-      { title: "Tabalit", src: "data/audio/Hamadine/tabalit.mp3" }
+      { title: "More than surviving", src: "data/audio/Hamadine/more-than-surviving.mp3" }
     ]
   }
 ];
