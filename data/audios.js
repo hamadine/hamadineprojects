7a9d22ba8b486e8df00686c8f3cecb4d98dae49b
hamadine <212258452+hamadine@users.noreply.github.com>
2025-08-06 15:25:02 +0100

    Update audios.js

--- data/audios.js
+++ data/audios.js
@@ -1,17 +1,20 @@
 const audios = [
   {
     album: "Hamadine",
-    pistes: [{ title: "A Chaque Instant Tamalat By Hamadine", src: "data/audio/Hamadine/a_chaque_instant_tamalat_by_hamadine.mp3", lyrics: null },
-  { title: "Ce Que Dieu M A Laisse", src: "data/audio/Hamadine/ce_que_dieu_m_a_laisse.mp3", lyrics: null },
-  { title: "Hamadine Dis Moi", src: "data/audio/Hamadine/hamadine_dis_moi.mp3", lyrics: null },
-  { title: "Hommage A Moussa", src: "data/audio/Hamadine/hommage_a_moussa.mp3", lyrics: null },
-  { title: "More Than Survivingofficial", src: "data/audio/Hamadine/more_than_survivingofficial.mp3", lyrics: null },
-  { title: "Rien Qu Un Homme", src: "data/audio/Hamadine/rien_qu_un_homme.mp3", lyrics: null },
-  { title: "Tabalitarabicversion", src: "data/audio/Hamadine/tabalitarabicversion.mp3", lyrics: null },
-  { title: "Tamalatacousticversion", src: "data/audio/Hamadine/tamalatacousticversion.mp3", lyrics: null },
-  { title: "Ton Daron Ton Monde Amira", src: "data/audio/Hamadine/ton_daron_ton_monde_amira.mp3", lyrics: null },
-  { title: "Verba Arma Nostra", src: "data/audio/Hamadine/verba_arma_nostra.mp3", lyrics: null },
-  { title: "Voix Du Lien Bridge Of Hope", src: "data/audio/Hamadine/voix_du_lien_bridge_of_hope.mp3", lyrics: null },
+    pistes: [
+      { title: "A Chaque Instant Tamalat By Hamadine", src: "data/audio/Hamadine/a_chaque_instant_tamalat_by_hamadine.mp3", lyrics: null },
+      { title: "Ce Que Dieu M A Laisse", src: "data/audio/Hamadine/ce_que_dieu_m_a_laisse.mp3", lyrics: null },
+      { title: "Hamadine Dis Moi", src: "data/audio/Hamadine/hamadine_dis_moi.mp3", lyrics: null },
+      { title: "Hommage A Moussa", src: "data/audio/Hamadine/hommage_a_moussa.mp3", lyrics: null },
+      { title: "More Than Survivingofficial", src: "data/audio/Hamadine/more_than_survivingofficial.mp3", lyrics: null },
+      { title: "Rien Qu Un Homme", src: "data/audio/Hamadine/rien_qu_un_homme.mp3", lyrics: null },
+      { title: "Tabalitarabicversion", src: "data/audio/Hamadine/tabalitarabicversion.mp3", lyrics: null },
+      { title: "Tamalatacousticversion", src: "data/audio/Hamadine/tamalatacousticversion.mp3", lyrics: null },
+      { title: "Ton Daron Ton Monde Amira", src: "data/audio/Hamadine/ton_daron_ton_monde_amira.mp3", lyrics: null },
+      { title: "Verba Arma Nostra", src: "data/audio/Hamadine/verba_arma_nostra.mp3", lyrics: null },
+      { title: "Voix Du Lien Bridge Of Hope", src: "data/audio/Hamadine/voix_du_lien_bridge_of_hope.mp3", lyrics: null },
+      { title: "Dis Moi Remix", src: "data/audio/Hamadine/dis_moi_remix.mp3", lyrics: null },
+      { title: "What He Left Me", src: "data/audio/Hamadine/what_he_left_me.mp3", lyrics: null }
     ]
   }
 ];
